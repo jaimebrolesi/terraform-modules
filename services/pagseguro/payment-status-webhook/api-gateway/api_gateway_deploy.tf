3156697ffb33440265901ed40d48c17e05ca8e6a
Jaime Brolesi <jaime.brolesi@brainweb.com.br>
2020-11-22 19:49:54 -0300

    fixed depends_on strategy used by pagseguro-payment-status-webhook

--- services/pagseguro/payment-status-webhook/api-gateway/api_gateway_deploy.tf
+++ services/pagseguro/payment-status-webhook/api-gateway/api_gateway_deploy.tf
@@ -12,5 +12,13 @@ resource "aws_api_gateway_deployment" "api_gateway_deploy" {
     create_before_destroy = true
   }
 
-  depends_on = [aws_api_gateway_integration.api_gateway_integration]
+  depends_on = [
+    aws_api_gateway_integration.api_gateway_integration,
+    aws_api_gateway_integration_response.http2xx,
+    aws_api_gateway_integration_response.http400,
+    aws_api_gateway_integration_response.http403,
+    aws_api_gateway_integration_response.http404,
+    aws_api_gateway_integration_response.http415,
+    aws_api_gateway_integration_response.http5xx,
+  ]
 }
