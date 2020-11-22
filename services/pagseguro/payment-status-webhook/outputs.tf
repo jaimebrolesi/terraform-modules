b4ec0b8d998d943f23cb11987888d1d955ba6cf4
Jaime Brolesi <jaime.brolesi@brainweb.com.br>
2020-11-22 14:23:23 -0300

    adjusted outputs for payment-status-webhook

--- services/pagseguro/payment-status-webhook/outputs.tf
+++ services/pagseguro/payment-status-webhook/outputs.tf
@@ -1,9 +1,9 @@
 output "queue_name" {
-  value       = module.simple_queue.name
+  value       = module.simple_queue.queue_name
   description = "The name of the queue that Api Gateway sends the payments updates"
 }
 
 output "iam_role_arn" {
-  value       = module.iam_role.role.arn
+  value       = module.iam_role.iam_role_arn
   description = "The arn of iam role used by Api Gateway Integration"
 }
