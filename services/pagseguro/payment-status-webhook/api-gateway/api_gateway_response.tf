d3a84daa9cf3407c7e4b18f48ebd55b488f1ea8b
Jaime Brolesi <jaime.brolesi@brainweb.com.br>
2020-11-22 21:23:19 -0300

    fixed depends_on strategy used by pagseguro-payment-status-webhook

--- services/pagseguro/payment-status-webhook/api-gateway/api_gateway_response.tf
+++ services/pagseguro/payment-status-webhook/api-gateway/api_gateway_response.tf
@@ -1,132 +1,20 @@
-resource "aws_api_gateway_method_response" "http200" {
+resource "aws_api_gateway_method_response" "method_response" {
+  for_each = var.response_mapping
+
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   resource_id = aws_api_gateway_resource.notifications_path.id
   http_method = aws_api_gateway_method.api_gateway_method.http_method
-  status_code = 200
+  status_code = each.key
 
   depends_on = [aws_api_gateway_integration.api_gateway_integration]
 }
 
-resource "aws_api_gateway_method_response" "http400" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.notifications_path.id
-  http_method = aws_api_gateway_method.api_gateway_method.http_method
-  status_code = 400
-
-  depends_on = [
-    aws_api_gateway_method_response.http200,
-    aws_api_gateway_integration.api_gateway_integration
-  ]
-}
-
-resource "aws_api_gateway_method_response" "http403" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.notifications_path.id
-  http_method = aws_api_gateway_method.api_gateway_method.http_method
-  status_code = 403
-
-  depends_on = [aws_api_gateway_method_response.http400,
-    aws_api_gateway_integration.api_gateway_integration
-  ]
-}
-
-resource "aws_api_gateway_method_response" "http404" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.notifications_path.id
-  http_method = aws_api_gateway_method.api_gateway_method.http_method
-  status_code = 404
-
-  depends_on = [aws_api_gateway_method_response.http403,
-  aws_api_gateway_integration.api_gateway_integration]
-}
-
-resource "aws_api_gateway_method_response" "http415" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.notifications_path.id
-  http_method = aws_api_gateway_method.api_gateway_method.http_method
-  status_code = 415
-
-  depends_on = [aws_api_gateway_method_response.http404,
-  aws_api_gateway_integration.api_gateway_integration]
-}
-
-resource "aws_api_gateway_method_response" "http500" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.notifications_path.id
-  http_method = aws_api_gateway_method.api_gateway_method.http_method
-  status_code = 500
-
-  depends_on = [aws_api_gateway_method_response.http415,
-    aws_api_gateway_integration.api_gateway_integration
-  ]
-}
-
 resource "aws_api_gateway_integration_response" "http2xx" {
+  for_each = var.response_mapping
+
   rest_api_id       = aws_api_gateway_rest_api.api_gateway.id
   resource_id       = aws_api_gateway_resource.notifications_path.id
   http_method       = aws_api_gateway_method.api_gateway_method.http_method
-  status_code       = aws_api_gateway_method_response.http200.status_code
-  selection_pattern = "^2[0-9][0-9]"
-
-  depends_on = [aws_api_gateway_method_response.http200]
-}
-
-resource "aws_api_gateway_integration_response" "http400" {
-  rest_api_id       = aws_api_gateway_rest_api.api_gateway.id
-  resource_id       = aws_api_gateway_resource.notifications_path.id
-  http_method       = aws_api_gateway_method.api_gateway_method.http_method
-  status_code       = aws_api_gateway_method_response.http400.status_code
-  selection_pattern = aws_api_gateway_method_response.http400.status_code
-
-  depends_on = [
-    aws_api_gateway_method_response.http400
-  ]
-}
-
-resource "aws_api_gateway_integration_response" "http403" {
-  rest_api_id       = aws_api_gateway_rest_api.api_gateway.id
-  resource_id       = aws_api_gateway_resource.notifications_path.id
-  http_method       = aws_api_gateway_method.api_gateway_method.http_method
-  status_code       = aws_api_gateway_method_response.http403.status_code
-  selection_pattern = aws_api_gateway_method_response.http403.status_code
-
-  depends_on = [
-    aws_api_gateway_method_response.http403
-  ]
-}
-
-resource "aws_api_gateway_integration_response" "http404" {
-  rest_api_id       = aws_api_gateway_rest_api.api_gateway.id
-  resource_id       = aws_api_gateway_resource.notifications_path.id
-  http_method       = aws_api_gateway_method.api_gateway_method.http_method
-  status_code       = aws_api_gateway_method_response.http404.status_code
-  selection_pattern = aws_api_gateway_method_response.http404.status_code
-
-  depends_on = [
-    aws_api_gateway_method_response.http404
-  ]
-}
-
-resource "aws_api_gateway_integration_response" "http415" {
-  rest_api_id       = aws_api_gateway_rest_api.api_gateway.id
-  resource_id       = aws_api_gateway_resource.notifications_path.id
-  http_method       = aws_api_gateway_method.api_gateway_method.http_method
-  status_code       = aws_api_gateway_method_response.http415.status_code
-  selection_pattern = aws_api_gateway_method_response.http415.status_code
-
-  depends_on = [
-    aws_api_gateway_method_response.http415
-  ]
-}
-
-resource "aws_api_gateway_integration_response" "http5xx" {
-  rest_api_id       = aws_api_gateway_rest_api.api_gateway.id
-  resource_id       = aws_api_gateway_resource.notifications_path.id
-  http_method       = aws_api_gateway_method.api_gateway_method.http_method
-  status_code       = aws_api_gateway_method_response.http500.status_code
-  selection_pattern = "^5[0-9][0-9]"
-
-  depends_on = [
-    aws_api_gateway_method_response.http500
-  ]
+  status_code       = values(aws_api_gateway_method_response.method_response)[each.key].status_code
+  selection_pattern = each.value
 }
--- services/pagseguro/payment-status-webhook/api-gateway/variables.tf
+++ services/pagseguro/payment-status-webhook/api-gateway/variables.tf
@@ -39,3 +39,20 @@ variable "environment" {
   description = "The environment to use for the api gateway deployment resources (e.g. Should be stage or prod)"
   type        = string
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "response_mapping" {
+  description = "A map of integration responses status (e.g. {\"200\" = \"^2[0-9][0-9]\"})"
+  type        = map
+
+  default = {
+    200 = "^2[0-9][0-9]",
+    400 = "400",
+    403 = "403",
+    415 = "415",
+    500 = "^5[0-9][0-9]"
+  }
+}
